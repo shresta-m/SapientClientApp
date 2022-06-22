a2dd053ef2fedfa70baf572f80e58298f947432c
shresta-m <coe18b048@iiitdm.ac.in>
2022-06-22 14:04:31 +0530

    changed B.java by user5

--- src/B.java
+++ src/B.java
@@ -1,5 +1,6 @@
 public class B {
     public static void main(String[] args){
-        System.out.println("Hello world");
+        System.out.println("Hello world by User5!!####");
+        System.out.println("Heyy");
     }
 }

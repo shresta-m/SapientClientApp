89a520613df42a8f4256297435e7f1ae9ca59e7b
shresta-m <coe18b048@iiitdm.ac.in>
2022-06-22 13:12:15 +0530

    modified A.java by user1

--- src/A.java
+++ src/A.java
@@ -15,8 +15,8 @@ public class A {
         }
     }
     public static void main(String[] args){
-        System.out.println("Hello world");
-        boolean ip =false;
+        System.out.println("Hello world!!!!!");
+        boolean ip =true;
         System.out.println(trueOrFalse(ip));
     }
 }
